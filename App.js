636e8c16563fc2b3d92763f74325696632d3858e
marikben <70907667+marikben@users.noreply.github.com>
2021-01-31 18:17:47 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput, Image} from 'react-native';
 
 export default function App() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+  const buttonPressed = () =>{
+    //for some reason buttonPressed2 didn't need the parseInt but this one did (to do the math.function)
+    setResult(parseInt(num1)+parseInt(num2))
+  }
+  const buttonPressed2 = () =>{
+  setResult(num1-num2)
+}
+  const styles = StyleSheet.create({
+    alerttext:{
+      fontSize:18,
+      color:'red'
+    },
+    
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result {result}</Text>
+      <TextInput 
+      style={{width:200, borderColor:'gray', borderWidth:1}}
+      keyboardType = 'numeric'
+      onChangeText={num1 => setNum1(num1)} value={num1}/>
+      <TextInput 
+      style={{width:200, borderColor:'gray', borderWidth:1}}
+      keyboardType = 'numeric'
+      onChangeText={num2 => setNum2(num2)}value={num2}/>
+      <Button onPress={buttonPressed} title="+"/>
+      <Button onPress={buttonPressed2} title="-"/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
