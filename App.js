e8e300056fd476a0ec68a40c6776b94f1a13cf28
marikben <70907667+marikben@users.noreply.github.com>
2021-01-31 20:20:05 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, Alert, TextInput, Image} from 'react-native';
+import { FlatList, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
   const [num1, setNum1] = useState('');
   const [num2, setNum2] = useState('');
   const [result, setResult] = useState('');
+  const [data, setData] = useState([]);
+ 
   const buttonPressed = () =>{
     //for some reason buttonPressed2 didn't need the parseInt but this one did (to do the math.function)
     setResult(parseInt(num1)+parseInt(num2))
+    setData([...data, {key:num1+'+'+num2+'='+(parseInt(num1)+parseInt(num2))}]);
   }
   const buttonPressed2 = () =>{
   setResult(num1-num2)
+  setData([...data, {key:num1+'-'+num2+'='+(num1-num2)}]);
 }
-  const styles = StyleSheet.create({
-    alerttext:{
-      fontSize:18,
-      color:'red'
-    },
-    
-  });
+
   return (
-    <View style={styles.container}>
-      <Text>Result {result}</Text>
+    <View>
+      <Text>Result: {result}</Text>
       <TextInput 
       style={{width:200, borderColor:'gray', borderWidth:1}}
       keyboardType = 'numeric'
@@ -33,7 +30,10 @@ export default function App() {
       onChangeText={num2 => setNum2(num2)}value={num2}/>
       <Button onPress={buttonPressed} title="+"/>
       <Button onPress={buttonPressed2} title="-"/>
+      <Text>History</Text>
+      <FlatList 
+      data={data}
+      renderItem={({item}) =><Text>{item.key}</ Text>}/>
     </View>
   );
 }
-
